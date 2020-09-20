0131909ef8ee747bc9c801dfe04980644924f812
devafrica <cdevafrica@gmail.com>
2020-09-20 09:44:13 +0200

    New API Explorer

---
+++ dist/js/config.js
@@ -0,0 +1,16 @@
+const ExplorerConfig = {
+  apiBaseUrl: 'https://restapi.cryptopay.org.za',
+  ticker: 'CPA',
+  maxSupply: 10000000000000,
+  emissionSpeed: 19,
+  decimalPoints: 6,
+  blockTargetTime: 30,
+  startYear: 2020,
+  genesisTimestamp: 1512800692,
+  emissionCurveInterval: 20000,
+  emissionCurveDataPoints: 15000,
+  addressPrefix: 129,
+  forkHeights: [
+    100000
+  ]
+}
